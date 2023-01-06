145a1ffca1664007a2914deb2906722544e7d312
Rhian Moura <rhian.moura@pagonxt.com>
2023-01-06 17:09:45 -0300

    Arrumando data, eliminando package-lock.json e atualizando .dockerignore

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -9,7 +9,7 @@ function createUserUseCase (name, email, password) {
         name: name,
         email: email,
         password: password,
-        createdDate: new Date().toLocaleDateString('pt-BR')
+        createdDate: new Date().toISOString().substring(0,10)
     })
 
     return users[users.length - 1];
