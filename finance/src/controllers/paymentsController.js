d0b371e8ea7b513d5963bc6b419c14495240feb0
Rhian Moura <rhian.moura@pagonxt.com>
2023-02-09 15:38:18 -0300

    Adding functionality listPaymentById on method get

--- finance/src/controllers/paymentsController.js
+++ finance/src/controllers/paymentsController.js
@@ -1,15 +1,40 @@
 const db = require('../models');
 
 class PaymentsController {
-    static async createPayment (req, res) {
-        const payment = {...req.body, status: 'CRIADO'};
-        try {
-          const newPayment = await db.Payments.create(payment);
-          return res.status(201).set('Location', `/payments/${newPayment.id}`).json({id: newPayment.id, status: newPayment.status});
-        } catch (error) {
-          return res.status(500).json(error.message);
-        }
-      }
+
+  static async listPaymentById (req, res) {
+    const { id } = req.params
+    try {
+      const payment = await db.Payments.findOne({ where: { id: Number(id) } })
+      return res.status(200)
+      .json({
+        id: payment.id,
+        valor: payment.valor, 
+        nome: payment.nome,
+        numeroCartao: payment.numeroCartao,
+        expiracaoCartao: payment.expiracaoCartao,
+        status: payment.status,
+        createdAt: payment.createdAt,
+        updatedAt: payment.updatedAt
+      })
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
+  }
+
+  static async createPayment (req, res) {
+    const payment = {...req.body, status: 'CRIADO'};
+    try {
+      const newPayment = await db.Payments.create(payment);
+      return res.status(201).set('Location', `/payments/${newPayment.id}`)
+      .json({
+        id: newPayment.id, 
+        status: newPayment.status
+      });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+}
 
 module.exports = PaymentsController;
--- finance/src/routes/paymentsRoutes.js
+++ finance/src/routes/paymentsRoutes.js
@@ -4,6 +4,7 @@ const PaymentsController = require('../controllers/paymentsController');
 const router = Router();
 
 router
+    .get("/api/admin/payments/:id", PaymentsController.listPaymentById)
     .post("/api/admin/payments", PaymentsController.createPayment)
 
 module.exports = router;
